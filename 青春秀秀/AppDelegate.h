fb64b682ce1642a25e5328152eec53241532517f
小王 <luluwang@luluwang-macbook-pro.local>
2017-12-06 22:02:10 +0800

    Initial Commit

---
+++ 青春秀秀/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  青春秀秀
+//
+//  Created by luluwang on 2017/12/6.
+//  Copyright © 2017年 luluwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 青春秀秀/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  青春秀秀
+//
+//  Created by luluwang on 2017/12/6.
+//  Copyright © 2017年 luluwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
